6118e990f12abe3a2a351dbb7dad7ef305dcdaee
Onix <piyvoda20033@gmail.com>
2026-04-06 22:53:57 +0300

    fixed deploy bug

--- components/TransactionsHistoryNav/TransactionsHistoryNav.tsx
+++ components/TransactionsHistoryNav/TransactionsHistoryNav.tsx
@@ -1,41 +1,25 @@
-'use client';
+"use client";
 
 import css from "./TransactionsHistoryNav.module.css";
 import Button from "../Button/Button";
 import { useRouter, usePathname } from "next/navigation";
 
-interface Props { 
+interface Props {
   onClose?: () => void;
 }
 
 export default function TransactionsHistoryNav({ onClose }: Props) {
-    const router = useRouter();
-    const pathname = usePathname();
+  const router = useRouter();
+  const pathname = usePathname();
   const isActive = (path: string) => pathname === path;
 
   const handleNavClick = (path: string) => {
     router.push(path);
-    if (onClose) onClose()
+    if (onClose) onClose();
   };
 
-<<<<<<< Header(logika-profile)
- 
-    return (
-      <nav className={css.nav}>
-      <Button
-        className={`${css.navItem} ${isActive("/transactions/history/expense") ? css.activeBorder : ""}`}
-        text="All Expense"
-        onClick={() => handleNavClick("/transactions/history/expense")} 
-        color={isActive("/transactions/expense") ? "green" : "dark"}
-      />
-      <Button
-        className={`${css.navItem} ${isActive("/transactions/history/income") ? css.activeBorder : ""}`}
-        text="All Income"
-        onClick={() => handleNavClick("/transactions/history/income")} 
-        color={isActive("/transactions/income") ? "green" : "dark"}
-=======
-    return (
-      <nav className={css.nav}>
+  return (
+    <nav className={css.nav}>
       <Button
         className={`${css.navItem} ${isActive("/transactions/history/expenses") ? css.activeBorder : ""}`}
         text="All Expense"
@@ -47,8 +31,7 @@ export default function TransactionsHistoryNav({ onClose }: Props) {
         text="All Income"
         onClick={() => handleNavClick("/transactions/history/incomes")}
         color={isActive("/transactions/history/incomes") ? "green" : "dark"}
->>>>>>> main
       />
     </nav>
-    )
+  );
 }
