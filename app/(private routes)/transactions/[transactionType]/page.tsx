b897b02a282573d7b76a13b0ed16647d75f05a7e
Русальчук Андрей <anrusalchuk@gmail.com>
2026-04-06 13:20:51 +0300

    transactions chart

--- app/(private routes)/transactions/[transactionType]/page.tsx
+++ app/(private routes)/transactions/[transactionType]/page.tsx
@@ -1,3 +1,19 @@
-export default function ComponentName() {
-  return <div></div>;
+import TransactionsTitle from '@/components/TransactionsTitle/TransactionsTitle';
+import TransactionsTotalAmount from '@/components/TransactionsTotalAmount/TransactionsTotalAmount';
+import TransactionsChart from '@/components/TransactionsChart/TransactionsChart';
+import styles from './page.module.css';
+
+export default function MainTransactionsPage() {
+  return (
+    <div className="container">
+      <main className={styles.page}>
+        <div className={styles.left}>
+          <TransactionsTitle />
+          <TransactionsTotalAmount />
+          <TransactionsChart />
+        </div>
+        <div className={styles.right}>{}</div>
+      </main>
+    </div>
+  );
 }
